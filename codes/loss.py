4984b3f0af10c3de4765376332564883bcba6cb1
张卡尔 <zke15@mails.tsinghua.edu.cn>
2017-10-25 12:25:44 +0800

    update report

--- codes/loss.py
+++ codes/loss.py
@@ -25,30 +25,9 @@ class SoftmaxCrossEntropyLoss(object):
         h = h / s
         self._saved_h = h
         e = np.mean(-np.sum(target * np.log(h), axis=1))    
-        # std = self.forward_std(input, target)
-        # if e == std:
-        #     print "loss good"
-        # else:
-        #     print "!!!!!!!!!!! loss bad"
         return e
 
     def backward(self, input, target):
         '''Your codes here'''
         h = self._saved_h
-        # std = self.backward_std(input, target)
-        # out = (h - target) / len(input)
-        # if out.all() == std.all():
-        #     print "loss good"
-        # else:
-        #     print "!!!!!!!!!!! loss bad"
         return (h - target) / len(input)
-    
-    def forward_std(self, input, target):
-        input_exp = np.exp(input)
-        h = np.divide(input_exp.T, np.sum(input_exp, 1)).T
-        self._saved_h = h
-        return np.mean(-np.sum(np.multiply(np.log(h), target), 1))
-
-    def backward_std(self, input, target):
-        h = self._saved_h
-        return (h - target) / target.shape[0]
--- result/show_result.py
+++ result/show_result.py
@@ -2,8 +2,7 @@ import cPickle
 import matplotlib.pyplot as plt
 
 filename1 = "init_std=0.1"
-filename2 = "lr=0.01"
-'''
+filename2 = "init_std=1"
 with open(filename1+'.data', "r") as f:
     train_loss_list_1=cPickle.load(f)
     train_acc_list_1=cPickle.load(f)
@@ -19,7 +18,7 @@ with open(filename2+'.data', "r") as f:
 name = filename1 + " vs " + filename2
 
 x = range(1, len(train_loss_list_1) + 1)
-plt.title(name + 'Train Loss')
+plt.title(name + ' Train Loss')
 plt.ylim((0, 0.8))
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
@@ -29,7 +28,7 @@ plt.legend(loc='upper right')
 plt.savefig(filename1 + "vs" + filename2+'_loss.png')
 
 plt.clf()
-plt.title(name + 'Train Accuracy')
+plt.title(name + ' Train Accuracy')
 plt.ylim((0.5, 1))
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
@@ -37,31 +36,7 @@ plt.plot(x, train_acc_list_1, 'r', label=filename1)
 plt.plot(x, train_acc_list_2, 'b', label=filename2)
 plt.legend(loc='lower right')
 plt.savefig(filename1 + "vs" + filename2+'_acc.png')
-'''
 
-filename = 'lr=0.01_2'
-with open(filename2+'.data', "r") as f:
-    train_loss_list=cPickle.load(f)
-    train_acc_list=cPickle.load(f)
-    test_loss_list=cPickle.load(f)
-    test_acc_list=cPickle.load(f)
-
-x = range(1, len(train_loss_list) + 1)
-plt.title('Train/Test Loss')
-plt.ylim((0, 0.8))
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.plot(x, train_loss_list, 'r', label='Train')
-plt.plot(x, test_loss_list, 'b', label='Test')
-plt.legend(loc='upper right')
-plt.savefig(filename+'_loss.png')
-
-plt.clf()
-plt.title('Train/Test Accuracy')
-plt.ylim((0.5, 1))
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.plot(x, train_acc_list, 'r', label='Train')
-plt.plot(x, test_acc_list, 'b', label='Test')
-plt.legend(loc='lower right')
-plt.savefig(filename+'_acc.png')
+result = ("%.3f&%.3f&%.2f\\%%&%.2f\\%%\n%.3f&%.3f&%.2f\\%%&%.2f\\%%") % (train_loss_list_1[-1], test_loss_list_1[-1],train_acc_list_1[-1]*100, test_acc_list_1[-1]*100,train_loss_list_2[-1], test_loss_list_2[-1],train_acc_list_2[-1]*100, test_acc_list_2[-1]*100)
+with open(filename1 + "vs" + filename2+'.txt', "w") as f:
+    f.write(result)
